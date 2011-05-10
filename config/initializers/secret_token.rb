15c24bccb4354b9d8a16184c0dedfec360bce392
Dmitry Ratnikov <ratnikov@gmail.com>
2011-05-10 18:20:10 -0400

    Initial generate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Armadillo::Application.config.secret_token = 'f364698219e5351acd484b638690b078466b2488c071dbabce4a1b848f3c3558bb05b0443a2117f01ff262d54258a64fe8fd5f827c9226f82693fee4254300fc'
